bba5b26134a7db813c6f62512a49cdca6f9708e9
Sashikantlvy <153577889+Sashikantlvy@users.noreply.github.com>
2024-03-01 06:56:11 +0530

    Update aiimage.py

--- MukeshRobot/modules/aiimage.py
+++ MukeshRobot/modules/aiimage.py
@@ -41,7 +41,7 @@ async def imagine_(b, message: Message):
     caption = f"""
 ✦ sᴜᴄᴇssғᴜʟʟʏ ɢᴇɴᴇʀᴀᴛᴇᴅ ✦
 
-❍ **ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ➛** [๛ᴀ ᴠ ɪ s ʜ ᴀ ༗](https://t.me/Avishaxbot)
+❍ **ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ➛** [ᴇᴍᴍᴀ](https://t.me/EMMA_X_ROBOT)
 ❍ **ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ ➛** {message.from_user.mention}
 """
     await m.delete()
